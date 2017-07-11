d964af0ff1066608a3f7087fa6242ce90a23236a
matsumoto <28219714@qq.com>
2017-07-11 17:26:56 +0800

    main page

--- wx/test.php
+++ wx/test.php
@@ -1,17 +1,66 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <title>绵职院校友会-信息录入</title>
-    <link href="./css/user_record_in.css" rel="stylesheet" type="text/css"/>
-    <link href="./css/mask.css" rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="/libs/jquery.js"></script>
+<!DOCTYPE html>
+<!-- saved from url=(0018)http://xyb.mnu.cn/ -->
+<html><!-- Mirrored from www.zi-han.net/theme/hplus/form_basic.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 20 Jan 2016 14:19:15 GMT --><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>绵阳职业技术学院校友调查问卷</title>
+    <meta name="keywords" content="绵阳职业技术学院校友用人单位调查问卷">
+    <meta name="description" content="绵阳职业技术学院校友用人单位调查问卷">
+
+    <link rel="shortcut icon" href="http://xyb.mnu.cn/favicon.ico">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/custom.css" rel="stylesheet">
+    <link href="./css/animate.min.css" rel="stylesheet">
+    <link href="./css/style.min.css" rel="stylesheet">
+    <style type="text/css">
+
+        .bg_img {
+            position:fixed;
+            width:100%;
+            height:100%;
+            top:0px;
+            left:0px;
+            z-index:-2;
+        }
+
+        .font_footer{
+            text-align: center;
+            font-size: 11px;
+        }
+    </style>
 </head>
-<body>
-<?php
-echo PHP_VERSION;
-?>
-</body>
-</html>
+
+<body class="gray-bg">
+
+<div class="wrapper wrapper-content animated fadeInRight" align="center">
+
+    <div class="row" style="max-width: 600px">
+        <div class="col-sm-12">
+            <img src="./images/header.png" width="100%" style="max-width: 600px">
+            <div class="ibox float-e-margins">
+                <div class="ibox-title">
+                    <h3 align="center">成都校友会调查问卷</h3>
+
+                </div>
+                <div class="ibox-content">
+                    <div class="alert alert-info">
+                        <a class="alert-link">企业调查问卷</a>
+                    </div>
+                    <div class="alert alert-info">
+                        <a class="alert-link font_small">校友调查问卷</a>
+                    </div>
+
+                    <div class="font_footer"> 成都涪诚汇科技有限公司<br>Power By Zengguoqiang<br></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<img class="bg_img" src="./images/bg.jpg">
+<script src="/libs/jquery.js"></script>
+<script src="/libs/bootstrap.min.js"></script>
+
+</body></html>
