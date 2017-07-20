4c3f32245fe1b6dcb104e9c5dc207464f6d19bfa
matsumoto <28219714@qq.com>
2017-07-20 19:09:06 +0800

    commit

---
+++ index2.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Acer
+ * Date: 2017/1/9
+ * Time: 15:01
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>绵阳职业技术学院-成都校友会</title>
+    <meta name="Keywords" content="成都校友会,聚首情">
+    <meta name="Description" content="绵阳职业技术学院-成都校友会">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/index.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="header">
+    <div class="header_content">
+        <div class="contact_us">
+            <img src="/images/tel.png"/>
+            <em class="tel_text">咨询热线</em>
+            <em class="tel_tel">400-000-00xxxx</em>
+        </div>
+    </div>
+</div>
+<div class="nav">
+    <div class="content">
+        <ul>
+            <li class="choose">网站首页</li>
+            <li>网站首页</li>
+            <li>网站首页</li>
+            <li>网站首页</li>
+        </ul>
+    </div>
+</div>
+
+
+<script type="text/javascript" src="/libs/jquery.min.js"></script>
+<script type="text/javascript" src="/libs/bootstrap.min.js"></script>
+</body>
+</html>
