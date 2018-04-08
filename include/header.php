a3cb03710829376fe90845263a0ebf4dc489cbb9
Martin <28219714@qq.com>
2018-04-08 21:05:56 +0800

    添加了logo ico

--- include/header.php
+++ include/header.php
@@ -1,6 +1,9 @@
 <!-- 顶部 开始 -->
 <div class="header">
     <div class="header_content">
+        <div class="banner_logo">
+            <img src="/images/banner_logo.png"/>
+        </div>
         <div class="contact_us">
             <img src="/images/tel.png"/>
             <em class="tel_text">咨询热线</em>
